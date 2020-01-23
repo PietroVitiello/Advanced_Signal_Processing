c72ba68a872edb1253953759530a988bd7bd49ce
pv09 <44298046+pv09@users.noreply.github.com>
2020-01-23 00:59:05 +0000

    computed ensamble average of realizations

--- Part 1/Subpart2/ASP_Part_1_2.m
+++ Part 1/Subpart2/ASP_Part_1_2.m
@@ -1,5 +1,3 @@
-% N = 1000; %number of discrete times
-% M = 10 %
 % 
 % n = 1:N;
 % x = sin(2*pi*5*0.001*n)';
@@ -8,6 +6,42 @@
 
 %% Set up
 clear all;
-x = rp1(100, 100);
-y = rp2(100, 100);
-v = rp3(100, 100);
+N = 100; %number of discrete times
+M = 100; %number of realizations
+x = rp1(M, N);
+y = rp2(M, N);
+v = rp3(M, N);
+
+%Plot a realization
+figure(), plot(1:N, x(1,:));
+
+for i = 1: N
+    ensamble_mx(i) = mean(x(:, i));
+    ensamble_sx(i) = std(x(:, i));
+    
+    ensamble_my(i) = mean(y(:, i));
+    ensamble_sy(i) = std(y(:, i));
+    
+    ensamble_mv(i) = mean(v(:, i));
+    ensamble_sv(i) = std(v(:, i));
+end
+
+figure()
+subplot(2, 1, 1)
+plot(1:N, ensamble_mx)
+subplot(2, 1, 2)
+plot(1:N, ensamble_sx)
+
+figure()
+subplot(2, 1, 1)
+plot(1:N, ensamble_my)
+subplot(2, 1, 2)
+plot(1:N, ensamble_sy)
+
+figure()
+subplot(2, 1, 1)
+plot(1:N, ensamble_mv)
+subplot(2, 1, 2)
+plot(1:N, ensamble_sv)
+
+
