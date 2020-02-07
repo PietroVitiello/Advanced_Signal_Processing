150d73a2beab4264776d507e4ffeb614ce6b5eb3
pv09 <vitiellocollegiosancarlo@gmail.com>
2020-02-07 20:18:10 +0000

    finished until 2.1

--- Part_2/Subpart1/ASP_Part_2_1.m
+++ Part_2/Subpart1/ASP_Part_2_1.m
@@ -1,30 +1,49 @@
 %% Noise set up
+clear all
 wnoise = randn(1, 1000);
 
-acf = xcorr(wnoise, 'unbiased');
-tao = [-999, 999];
+[acf, lags] = xcorr(wnoise, 'unbiased');
 
 %% Part 1
-%plot(lags, acf);
-title('Autocorrelation function of white Gaussian noise')
+plot(lags, acf);
+title('ACF of 1000 samples white Gaussian noise')
 ylabel('Amplitude (Au)')
 xlabel('relative distance between samples ({\tau})')
-%xlim([-999 999])
-stem(lags, acf);
+axis([-999 999 -inf inf])
+%stem(lags, acf);
 
 %% Part 2
+figure()
 plot(lags, acf);
-title('Autocorrelation function of white Gaussian noise')
+title('Zoomed ACF of 1000 samples white Gaussian noise')
 ylabel('Amplitude (Au)')
 xlabel('relative distance between samples ({\tau})')
-xlim([-50 50])
+axis([-50 50 -inf inf])
+
+figure()
+plot(lags, acf);
+title('Zoomed ACF of 1000 samples white Gaussian noise')
+ylabel('Amplitude (Au)')
+xlabel('relative distance between samples ({\tau})')
+axis([600 inf -inf inf])
 
 %% Part 4
-filtered_wnoise = filter(ones(9,1), 1, wnoise);
+filtered_wnoise = filter(2*ones(9,1), [1], wnoise);
+
+%% Part 4 plot filtered noise
 figure()
-stem(lags, acf);
+subplot(2, 1, 1)
+plot(1:1000, wnoise)
+subplot(2, 1, 2)
+plot(1:1000, filtered_wnoise)
+
+%% Part 4 plot filtered noise ACF
+[f_acf, f_lags] = xcorr(filtered_wnoise, 'unbiased');
+figure()
+stem(f_lags, f_acf);
 title('Autocorrelation function of filtered white Gaussian noise')
 ylabel('Amplitude (Au)')
 xlabel('relative distance between samples ({\tau})')
-xlim([-20 20])
+axis([-20 20 -inf inf])
+% xlim([-20 20])
 
