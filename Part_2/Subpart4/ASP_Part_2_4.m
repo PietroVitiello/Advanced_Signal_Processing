ca0618cd6846a3de07223fae278a1ba575546cfb
pv09 <44298046+pv09@users.noreply.github.com>
2020-02-29 01:19:41 +0000

    done part 2.4

--- Part_2/Subpart4/ASP_Part_2_4.m
+++ Part_2/Subpart4/ASP_Part_2_4.m
@@ -2,21 +2,27 @@ clear all
 close all
 
 load NASDAQ.mat
-prices = NASDAQ.Close;
+prices = zscore(NASDAQ.Close);
 dates = NASDAQ.Date;
+N = length(prices);
 
 %% Part 1 pcf
 p = 10;
 pcf_axis = 1:p;
 
+threshold = 1.96/sqrt(N);
+
 [a, ~, k] = aryule(prices, 10);
-figure()
+figure(), hold on
 stem(pcf_axis, -k)
+plot([1 p], [1 1]'*[threshold -threshold], 'r')
+title("Partial Correlation of NASDAQ prices data")
+xlabel("\tau")
+ylabel("PCF")
 
 %% Part 1 errors
 %% Part 4
-N = length(prices);
-p = 20;
+p = 10;
 norm_prices = zscore(prices);
 close_p = ones(N, p) .* norm_prices;
 
@@ -50,6 +56,40 @@ plot(p_axis, AIC)
 plot(p_axis, AIC_c)
 % plot(p_axis, cumulative)
 legend('Minimum Description Length', 'Akaike Information Criterion', 'corrected AIC') %, 'Cumulative Error Squared')
-title("Different criteria for loss calculation with respect to the model's order")
+title("Loss calculation with respect to the model's order for NASDAQ data")
 xlabel("model order p")
 ylabel("Loss function")
+
+%% Part 3
+close all
+
+sigma2 = 1:50:1001;
+n = sigma2;
+
+acf = xcorr(prices, 'unbiased');
+
+for i = 1:length(sigma2)
+    formula_sigma(i,:) = 1./n * (2*sigma2(1, i)^2);
+end
+
+figure()
+h = heatmap(n, sigma2, formula_sigma);
+h.Colormap = parula;
+h.ColorScaling = 'log';
+title("Heatmap for the variance of the noise variance")
+xlabel("N")
+ylabel("True variance of the driving noise")
+
+for i = 1:length(sigma2)
+    formula_a(i,:) = 1./n * (1/max(acf)) * sigma2(1, i);
+end
+
+figure()
+h2 = heatmap(n, sigma2, formula_a);
+h2.Colormap = parula;
+h2.ColorScaling = 'log';
+title('Heatmap for the variance of a1')
+xlabel("N")
+ylabel("True variance of the driving noise")
+
+
