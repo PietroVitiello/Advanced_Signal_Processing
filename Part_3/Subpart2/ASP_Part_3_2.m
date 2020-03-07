db10a7fc9dfcc1db642628ab7b84e0f9cf328aca
pv09 <44298046+pv09@users.noreply.github.com>
2020-03-07 00:17:46 +0000

    part 3.2

--- Part_3/Subpart2/ASP_Part_3_2.m
+++ Part_3/Subpart2/ASP_Part_3_2.m
@@ -28,22 +28,50 @@ ylabel('Amplitude (Au)')
 %% Part 1
 [h,w]=freqz(1,[1 0.9],512);
 plot(w/(2*pi),abs(h).^2)
- 
+
 %% Part 2
 addpath(genpath([fileparts(pwd), '\Subpart1']));
 
-N = N-40;
+N = 1024;
 estimated_per = pgm(y);
 pgm_axis = 0:1/N:(N-1)/N;
 
 figure()
 plot(pgm_axis, estimated_per)
-xlim([0 0.5])
+% xlim([0 0.5])
+axis([0 0.5 -inf inf])
 
 %% Part 3
 figure()
 plot(pgm_axis, estimated_per)
-xlim([0.4 0.5])
+%xlim([0.4 0.5])
+axis([0.4 0.5 -inf inf])
 
+%% Window
+rect = zeros(1024, 1);
+rect(462:512, 1) = 1;
+sinc = fftshift(ifft(rect));
+plot(abs(sinc));
+
+sgangersber = conv(y, sinc);
+pasgar = pgm(sgangersber);
+plot(pasgar)
+
+%% Part 4
+ry = xcorr(y, 'unbiased');
+acf_axis = -length(y)+1 : length(y)-1;
+a1_est = -ry(1, ceil(end/2)+1)/ry(1, ceil(end/2));
+var_est = ry(1, ceil(end/2)) + a1_est*ry(1, ceil(end/2)+1);
+
+N = 1024;
+freq = 0:1/N:(N-1)/N;
+Py = var_est ./ abs(1 + a1_est * exp(-2*1i*pi.*freq)).^2;
+
+[h,w]=freqz(var_est,[1 a1_est],512);
+figure()
+plot(w/(2*pi),abs(h).^2)
+
+figure()
+plot(freq, Py)
 
 
---
+++ Part_3/Subpart2/ASP_Part_3_2_sunspot.m
@@ -0,0 +1,54 @@
+%% Part 5
+clear all
+close all
+clc
+
+load sunspot.dat
+sun = filter(1, [1 0.9], sunspot(:,2));
+
+% Ideal psd
+[h_ideal,w_ideal]=freqz(1,[1 0.9],512);
+
+%estimated psd using pgm
+addpath(genpath([fileparts(pwd), '\Subpart1']));
+
+N = length(sun);
+estimated_per = pgm(sun);
+pgm_axis = 0:1/N:(N-1)/N;
+
+% model psd
+ry = xcorr(sun, 'unbiased');
+acf_axis = -length(sun)+1 : length(sun)-1;
+a_est = aryule(sun, 10);
+var_est = ry(ceil(end/2)) + a_est(2)*ry(ceil(end/2)+1);
+% addpath(genpath([fileparts(pwd), '\Subpart1']));
+% 
+% N = length(sun);
+% estimated_per = pgm(sun);
+% pgm_axis = 0:1/N:(N-1)/N;
+% 
+% % model psd
+% ry = xcorr(sun, 'unbiased');
+% acf_axis = -length(sun)+1 : length(sun)-1;
+% a1_est = -ry(ceil(end/2)+1)/ry(ceil(end/2));
+% var_est = ry(ceil(end/2)) + a1_est*ry(ceil(end/2)+1);
+
+% freq = 0:1/N:(N-1)/N;
+% Py = var_est ./ abs(1 + a1_est * exp(-2*1i*pi.*freq)).^2;
+
+% [h,w]=freqz(var_est,[1 a1_est],512);
+[h,w]=freqz(var_est,[a_est],512);
+
+% Plot the various psd
+figure()
+plot(w_ideal/(2*pi),abs(h_ideal).^2)
+title('ideal')
+
+figure()
+plot(w/(2*pi),abs(h).^2)
+title('model')
+
+figure()
+plot(pgm_axis, estimated_per)
+title('pgm')
+
